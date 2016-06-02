dfa12fe4c63804ec3beb43968785f8439984f97f
ondrejproch <ondrej.proch@gmail.com>
2016-06-02 19:01:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Ondrej? Just say HELLO to get started.')
+            return bot.say('So you want to learn about Ondrej? Just say hello to get started.')
                 .then(() => 'speak');
         }
     },
